d18fae6eb5957248e315a6c6d857c95330fdf952
David Liu <kucuyff@gmail.com>
2024-11-01 03:45:07 -0400

    Updated Code

--- script.js
+++ script.js
@@ -3,81 +3,77 @@ const heronsFormula = (a, b, c) => {
     return (1/4) * Math.sqrt(4 * a * a * b * b - Math.pow(a * a + b * b - c * c, 2));
 }
 
-document.getElementById('calculate-heron').addEventListener('click', function() {
-    var a = parseFloat(document.getElementById('side-a-heron').value);
-    var b = parseFloat(document.getElementById('side-b-heron').value);
-    var c = parseFloat(document.getElementById('side-c-heron').value);
-    document.getElementById('heron-result').value = heronsFormula(a, b, c);
+document.getElementById('heron-calculation').addEventListener('click', function() {
+    var a = parseFloat(document.getElementById('heron-a').value);
+    var b = parseFloat(document.getElementById('heron-b').value);
+    var c = parseFloat(document.getElementById('heron-c').value);
+    document.getElementById('heron-answer').value = heronsFormula(a, b, c);
 });
 
 // Ambiguous Case
 const ambiguousCase = (a, b, A) => {
     if (A == 90) {
-        return 'Right triangle';
+        return 'Right Triangle';
     }
     var h = b * Math.sin(A * (Math.PI / 180));
     if (A < 90) {
         console.log('a: ' + a + ' h: ' + h)
         if (a < h) {
-            return 'No triangle';
+            return 'No Triangle';
         } else if (a > h) {
-            return 'One triangle';
+            return 'One Triangle';
         } else if ((h < a) && (a < b)) {
-            return 'Two triangles (ambiguous case)';
+            return 'Two Triangles (Ambiguous Case)';
         } else {
-            return 'No solution';
+            return 'No Solution';
         }
     }
     else  {
         if ((a < b)) {
-            return 'No triangle';
+            return 'No Triangle';
         } else if (a > b) {
-            console.log('One triangle obtuse')
-            return 'One triangle';
+            return 'One Triangle';
         } else {
-            return 'No solution';
+            return 'No Solution';
         }
     }
 }
 
 
-document.getElementById('calculate-ambiguous').addEventListener('click', function() {
-    var a = parseFloat(document.getElementById('side-a-ambiguous').value);
-    var b = parseFloat(document.getElementById('side-b-ambiguous').value);
-    var A = parseFloat(document.getElementById('angle-a-ambiguous').value);
-    document.getElementById('triangle-type-result').value = ambiguousCase(a, b, A);
+document.getElementById('ambiguous-calculation').addEventListener('click', function() {
+    var a = parseFloat(document.getElementById('ambiguous-side-a').value);
+    var b = parseFloat(document.getElementById('ambiguous-side-b').value);
+    var A = parseFloat(document.getElementById('ambiguous-angle-a').value);
+    document.getElementById('triangle-type').value = ambiguousCase(a, b, A);
 });
 
 
 // Newton's Method 
-const newtonsMethod = (g) => {
+const newtonsMethod = (a) => {
     function f(x) {
         return 6 * Math.pow(x, 4) - 13 * Math.pow(x, 3) - 18 * Math.pow(x, 2) + 7 * x + 6;
     }
     function fPrime(x) {
         return 24 * Math.pow(x, 3) - 39 * Math.pow(x, 2) - 36 * x + 7;
     }
-
     let maxIterations = 1000;
     let iterations = 0;
-
-    while (Math.abs(f(g)) > 0.0001 && iterations < maxIterations) {
-        g = g - f(g) / fPrime(g); // Newton's method update
+    while (Math.abs(f(a)) > 0.0001 && iterations < maxIterations) {
+        a = a - f(a) / fPrime(a); 
         iterations++;
     }
-
-    return g;
+    return a;
 }
 
-document.getElementById('calculate-newton').addEventListener('click', function() {
+document.getElementById('newton-calculation').addEventListener('click', function() {
     var g = parseFloat(document.getElementById('root-guess').value);
-    document.getElementById('root-approximation-result').value = newtonsMethod(g);
+    document.getElementById('root-approximation').value = newtonsMethod(g);
 });
 
 
-document.getElementById('calculate-newton').addEventListener('click', function() {
+document.getElementById('newton-calculation').addEventListener('click', function() {
     var g = parseFloat(document.getElementById('root-guess').value);
-    document.getElementById('root-approximation-result').value = newtonsMethod(g);
+    document.getElementById('root-approximation').value = newtonsMethod(g);
 });
 
 
@@ -104,11 +100,11 @@ const polynomialFunction = (coefficients, exponents) => {
     return result;
 }
 
-document.getElementById('calculate-polynomial').addEventListener('click', function() {
+document.getElementById('polynomial-calculation').addEventListener('click', function() {
     var coefficients = document.getElementById('coefficients').value.split(' ');
     var exponents = document.getElementById('exponents').value.split(' ');
     var xvalue = parseFloat(document.getElementById('x-value').value);
 
-    document.getElementById('polynomial-function-result').value = polynomialFunction(coefficients, exponents);
-    document.getElementById('polynomial-evaluation-result').value = polynomialEvaluation(coefficients, exponents, xvalue);
+    document.getElementById('polynomial-function').value = polynomialFunction(coefficients, exponents);
+    document.getElementById('polynomial-answer').value = polynomialEvaluation(coefficients, exponents, xvalue);
 });
